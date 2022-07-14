45843c538585d763b9367bc17977b47cd4c3d39b
Gifa Eriyanto <gifa.eriyanto@gmail.com>
2022-07-15 01:00:33 +0800

    feat(fonts): remove unused fonts

--- configuration.nix
+++ configuration.nix
@@ -84,9 +84,6 @@
   fonts = {
     fontDir.enable = true;
     fonts = with pkgs; [
-      fira
-      fira-code
-      fira-code-symbols
       (nerdfonts.override { fonts = [ "FiraCode" ]; })
     ];
   };
