d9e50a32c39a7f09871e072f58e0b14208671ae1
Gifa Eriyanto <gifa.eriyanto@gmail.com>
2022-07-15 00:29:09 +0800

    feat(starship): minimal config

--- home/shells.nix
+++ home/shells.nix
@@ -61,25 +61,6 @@
       settings = {
         add_newline = true;
         command_timeout = 1000;
-        cmd_duration = {
-          format = " [$duration]($style) ";
-          style = "bold #EC7279";
-          show_notifications = true;
-        };
-        nix_shell = {
-          format = " [$symbol$state]($style) ";
-        };
-        battery = {
-          full_symbol = "🔋 ";
-          charging_symbol = "⚡️ ";
-          discharging_symbol = "💀 ";
-        };
-        git_branch = {
-          format = "[$symbol$branch]($style) ";
-        };
-        gcloud = {
-          format = "[$symbol$active]($style) ";
-        };
       };
     };
   };
