493acee1e790c98985c591b262aa7e89d950360e
Gifa Eriyanto <gifa.eriyanto@gmail.com>
2022-11-13 07:11:08 +0800

    feat: add vim.nix

--- home/vim.nix
+++ home/vim.nix
@@ -1,11 +1,10 @@
-nixpkgs.overlays = [
-   (self: super: {
-     neovim = super.neovim.override {
-       viAlias = true;
-       vimAlias = true;
-     };
-  })
-];
+{ ... }:
 
-programs.neovim.enable = true;
-programs.neovim.viAlias = true;
+{
+  programs = {
+    neovim = {
+      enable = true;
+      viAlias = true;
+    };
+  };
+}
